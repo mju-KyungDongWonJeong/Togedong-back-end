1be2b446a9db51264db2c5a7748cb9a9fa3246f9
WonyuChoi <phillip07290@naver.com>
2023-10-29 00:38:34 +0900

    setting: init project

---
+++ src/test/java/com/togedong/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.togedong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
