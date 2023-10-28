f35b26bd09ce5e22e3ba8d47b1dcb6156e333d4e
WonyuChoi <phillip07290@naver.com>
2023-10-29 00:48:54 +0900

    feat: 유저 레포지토리 생성

---
+++ src/main/java/com/togedong/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.togedong.user.repository;
+
+import com.togedong.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
