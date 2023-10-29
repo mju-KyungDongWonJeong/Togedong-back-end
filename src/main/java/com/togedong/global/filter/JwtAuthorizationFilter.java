5461b573b62c6908ea5eb03105efcac69e9afaa8
WonyuChoi <phillip07290@naver.com>
2023-10-29 17:05:20 +0900

    feat: jwt로 필터 기능을 할 객체

---
+++ src/main/java/com/togedong/global/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,50 @@
+package com.togedong.global.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.util.PatternMatchUtils;
+import org.springframework.web.cors.CorsUtils;
+
+public class JwtAuthorizationFilter implements Filter {
+
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    private String[] whiteListURI = new String[]{"/auth/*", "/websocket-stomp/*", "/h2-console/*"};
+
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse,
+        final FilterChain filterChain)
+        throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String requestURI = request.getRequestURI();
+
+        if (checkWhiteList(requestURI) || CorsUtils.isPreFlightRequest(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String authorization = request.getHeader(AUTHORIZATION);
+
+        if (authorization == null || !authorization.startsWith(BEARER_PREFIX)) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "토큰이 존재하지 않습니다.");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean checkWhiteList(final String requestURI) {
+        return PatternMatchUtils.simpleMatch(whiteListURI, requestURI);
+    }
+}
