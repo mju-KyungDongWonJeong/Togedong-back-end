70aca62c7895068b568dac66ad9cda7e3308a542
WonyuChoi <phillip07290@naver.com>
2023-10-29 17:07:15 +0900

    feat: user id로 유저를 찾는 메서드

--- src/main/java/com/togedong/user/repository/UserRepository.java
+++ src/main/java/com/togedong/user/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.togedong.user.repository;
 
 import com.togedong.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
+    boolean existsByUserId(final String userId);
+
+    Optional<User> findByUserId(final String userId);
 }
