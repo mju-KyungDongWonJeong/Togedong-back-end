b5f53d1ea564b59bcddacded780aad97c7a0bde3
WonyuChoi <phillip07290@naver.com>
2023-11-17 19:03:04 +0900

    feat: 대시보드에 필요한 정적 객체

---
+++ src/main/java/com/togedong/exercise/Exercise.java
@@ -0,0 +1,16 @@
+package com.togedong.exercise;
+
+import lombok.Getter;
+
+@Getter
+public enum Exercise {
+
+    PUSH_UP("푸시업"),
+    SQUAT("스쿼트");
+
+    private final String name;
+
+    Exercise(final String name) {
+        this.name = name;
+    }
+}
