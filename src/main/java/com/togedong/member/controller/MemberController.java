53d31e05b75c89ff7c682903d275b448231290d9
WonyuChoi <phillip07290@naver.com>
2023-11-20 02:42:50 +0900

    feat: 운동 순위를 받을 API

--- src/main/java/com/togedong/member/controller/MemberController.java
+++ src/main/java/com/togedong/member/controller/MemberController.java
@@ -1,11 +1,13 @@
 package com.togedong.member.controller;
 
+import com.togedong.member.controller.dto.RecordResponse;
 import com.togedong.member.controller.dto.TotalChallengeResponse;
 import com.togedong.global.annotation.TokenInfo;
 import com.togedong.global.response.ResponseHandler;
 import com.togedong.member.controller.dto.DashBoardResponse;
 import com.togedong.member.entity.Member;
 import com.togedong.member.service.MemberService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -42,5 +44,11 @@ public class MemberController {
         TotalChallengeResponse challenges = memberService.getMemberChallenges(member,
             userName);
         return ResponseHandler.generateResponseWithoutMessage(HttpStatus.OK, challenges);
+    }
+
+    @GetMapping("/rank/{exerciseName}")
+    public ResponseEntity<Object> getRank(@PathVariable String exerciseName) {
+        List<RecordResponse> records = memberService.getExerciseRank(exerciseName);
+        return ResponseHandler.generateResponseWithoutMessage(HttpStatus.OK, records);
     }
 }
