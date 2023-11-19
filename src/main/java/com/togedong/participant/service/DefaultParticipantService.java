faeba1e7ffa96fc113f593e2d7926b29b67f09cb
WonyuChoi <phillip07290@naver.com>
2023-11-19 17:03:51 +0900

    feat: 챌린지와 멤버 관계 테이블 기능

---
+++ src/main/java/com/togedong/participant/service/DefaultParticipantService.java
@@ -0,0 +1,45 @@
+package com.togedong.participant.service;
+
+import static com.togedong.global.exception.ErrorCode.DUPLICATE_CHALLENGE;
+
+import com.togedong.challenge.entity.Challenge;
+import com.togedong.challenge.service.ChallengeService;
+import com.togedong.global.exception.CustomException;
+import com.togedong.member.entity.Member;
+import com.togedong.participant.entity.Participant;
+import com.togedong.participant.repository.ParticipantRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class DefaultParticipantService implements ParticipantService {
+
+    private final ParticipantRepository participantRepository;
+    private final ChallengeService challengeService;
+
+    @Override
+    @Transactional
+    public void participantChallenge(final Member member, final String challengeName) {
+        Challenge challenge = challengeService.findChallengeByName(challengeName);
+
+        if (alreadyParticipate(member, challenge)) {
+            throw new CustomException(DUPLICATE_CHALLENGE);
+        }
+
+        Participant participant = Participant.participateChallenge(member, challenge);
+        participantRepository.save(participant);
+    }
+
+    @Override
+    public boolean alreadyParticipate(final Member member, final Challenge challenge) {
+        return participantRepository.existsParticipantByMemberAndChallenge(member, challenge);
+    }
+
+    @Override
+    public int getChallengeParticipantCount(final String challengeName) {
+        return participantRepository.countByChallenge_Id(challengeName);
+    }
+}
---
+++ src/main/java/com/togedong/participant/service/ParticipantService.java
@@ -0,0 +1,13 @@
+package com.togedong.participant.service;
+
+import com.togedong.challenge.entity.Challenge;
+import com.togedong.member.entity.Member;
+
+public interface ParticipantService {
+
+    void participantChallenge(final Member member, final String challenge);
+
+    boolean alreadyParticipate(final Member member, final Challenge challenge);
+
+    int getChallengeParticipantCount(final String challengeName);
+}
