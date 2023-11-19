a2ae2421707139a8e96fe6c1c65b6a44f5836c1c
WonyuChoi <phillip07290@naver.com>
2023-11-20 02:45:24 +0900

    feat: 유저 중복 없이 순위를 반환해 주는 리포지토리 객체

--- src/main/java/com/togedong/record/repository/RecordRepository.java
+++ src/main/java/com/togedong/record/repository/RecordRepository.java
@@ -1,8 +1,11 @@
 package com.togedong.record.repository;
 
+import com.togedong.record.Exercise;
 import com.togedong.record.entity.ExerciseRecord;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecordRepository extends JpaRepository<ExerciseRecord, Long> {
 
+    List<ExerciseRecord> findDistinctByExercise(final Exercise exercise);
 }
