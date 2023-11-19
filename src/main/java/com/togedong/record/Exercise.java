4bbab01b7a0af8a72bdaea2fada982d40fb4960b
WonyuChoi <phillip07290@naver.com>
2023-11-20 02:44:04 +0900

    feat: 운동 이름을 받고 해당 운동 객체를 반환해 주는 메서드

--- src/main/java/com/togedong/record/Exercise.java
+++ src/main/java/com/togedong/record/Exercise.java
@@ -1,5 +1,9 @@
 package com.togedong.record;
 
+import static com.togedong.global.exception.ErrorCode.EXERCISE_NOT_FOUND;
+
+import com.togedong.global.exception.CustomException;
+import java.util.stream.Stream;
 import lombok.Getter;
 
 @Getter
@@ -12,5 +16,12 @@ public enum Exercise {
 
     Exercise(final String name) {
         this.name = name;
+    }
+
+    public static Exercise findExerciseByName(final String name) {
+        return Stream.of(Exercise.values())
+            .filter(exercise -> exercise.name().equals(name))
+            .findAny()
+            .orElseThrow(()->new CustomException(EXERCISE_NOT_FOUND));
     }
 }
