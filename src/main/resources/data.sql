4b5a87a9532a7dd449de0e9b8f6a04adb7edaec3
WonyuChoi <phillip07290@naver.com>
2023-11-20 14:23:12 +0900

    feat: 뱃지 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO Challenge (id,exercise,description) values ('pushUp','PUSH_UP','팔굽 일주일 동안 600개씩');
-INSERT INTO Challenge (id,exercise,description) values ('squat','SQUAT','스쿼트 일주일 동안 600개씩');
+INSERT INTO Challenge (id,exercise,description,badge) values ('pushUp','PUSH_UP','팔굽 일주일 동안 600개씩','PUSH_UP_BADGE');
+INSERT INTO Challenge (id,exercise,description,badge) values ('squat','SQUAT','스쿼트 일주일 동안 600개씩','SQUAT_BADGE');
