f5badbda8767a90f01805cf1dd16477ff7e9833f
WonyuChoi <phillip07290@naver.com>
2023-11-20 21:36:34 +0900

    feat: 운동 기록 등록 요청 역할을 서비스 객체가 수행

--- src/main/java/com/togedong/record/service/DefaultRecordService.java
+++ src/main/java/com/togedong/record/service/DefaultRecordService.java
@@ -1,19 +1,27 @@
 package com.togedong.record.service;
 
 import com.togedong.member.controller.dto.RecordResponse;
+import com.togedong.member.entity.Member;
+import com.togedong.participant.service.ParticipantService;
 import com.togedong.record.Exercise;
+import com.togedong.record.controller.dto.RecordRequest;
 import com.togedong.record.entity.ExerciseRecord;
 import com.togedong.record.repository.RecordRepository;
 import java.util.Comparator;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Slf4j
 public class DefaultRecordService implements RecordService {
 
     private final RecordRepository recordRepository;
+    private final ParticipantService participantService;
 
     @Override
     public List<RecordResponse> getRankByExercise(final Exercise exercise) {
@@ -23,5 +31,16 @@ public class DefaultRecordService implements RecordService {
         return ranks.stream()
             .map(ExerciseRecord::toDto)
             .toList();
+    }
+
+    @Override
+    @Transactional
+    public void addRecord(final Member member, final RecordRequest request) {
+        Exercise exercise = Exercise.findExerciseByName(request.exerciseName());
+        ExerciseRecord record = new ExerciseRecord(exercise, member, request.record());
+        member.addRecord(record);
+
+        participantService.checkParticipantArchive(member);
+        recordRepository.save(record);
     }
 }
