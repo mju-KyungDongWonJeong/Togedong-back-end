6d2a89915b2514ac057f78f1822a59697d3dfefb
WonyuChoi <phillip07290@naver.com>
2023-11-24 20:03:07 +0900

    feat: 게임방 엔티티 객체

---
+++ src/main/java/com/togedong/room/entity/Room.java
@@ -0,0 +1,67 @@
+package com.togedong.room.entity;
+
+import com.togedong.record.Exercise;
+import com.togedong.room.controller.dto.RoomCreateRequest;
+import com.togedong.room.controller.dto.RoomCreateResponse;
+import com.togedong.room.controller.dto.RoomResponse;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@AllArgsConstructor
+public class Room {
+
+    @Id
+    private String id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String managerName;
+
+    @Column
+    private int memberLimit;
+
+    @Enumerated(value = EnumType.STRING)
+    private Exercise exercise;
+
+    @Column
+    private String password;
+
+    @Column
+    private boolean hasPassword;
+
+    public static Room of(final RoomCreateRequest request, final String sessionId,
+        final String managerName, final Exercise exercise) {
+        return new Room(sessionId, managerName, request.title(),
+            request.memberLimit(), exercise,
+            request.password(),
+            request.hasPassword());
+    }
+
+    public RoomCreateResponse toCreateDto(final String connectionToken) {
+        return new RoomCreateResponse(id, title, managerName, memberLimit, connectionToken,
+            exercise.getName());
+    }
+
+    public RoomResponse toCommonDto() {
+        return new RoomResponse(title, managerName, memberLimit, exercise.getName(), hasPassword);
+    }
+
+    public boolean canJoin(final String password) {
+        if (!hasPassword) {
+            return true;
+        }
+        return this.password.equals(password);
+    }
+}
