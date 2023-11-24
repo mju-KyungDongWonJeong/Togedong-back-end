3dffbba5ea890025dde27c5f18a460db6272fa93
WonyuChoi <phillip07290@naver.com>
2023-11-24 20:02:28 +0900

    refactor: 주입할 수 있도록 final 키워드 사용

--- src/main/java/com/togedong/global/config/WebConfig.java
+++ src/main/java/com/togedong/global/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.togedong.global.config;
 import com.togedong.global.annotation.TokenInfoResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    private TokenInfoResolver resolver;
+    private final TokenInfoResolver resolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
