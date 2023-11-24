08ca52b1aa6b880b57c7f7de52b6070a90927723
WonyuChoi <phillip07290@naver.com>
2023-11-24 20:04:36 +0900

    feat: 게임방 리포지토리 객체

---
+++ src/main/java/com/togedong/room/repository/RoomRepository.java
@@ -0,0 +1,11 @@
+package com.togedong.room.repository;
+
+import com.togedong.record.Exercise;
+import com.togedong.room.entity.Room;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room,String> {
+
+    List<Room> findByExercise(final Exercise exercise);
+}
