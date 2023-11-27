6ba3d8cbab6d99828c457f2723b13bf037104558
WonyuChoi <phillip07290@naver.com>
2023-11-27 14:35:09 +0900

    feat: 총 개수에서 뱃지 획득 개수의 비율 계산 메서드

--- src/main/java/com/togedong/badge/Badge.java
+++ src/main/java/com/togedong/badge/Badge.java
@@ -6,11 +6,18 @@ import lombok.Getter;
 public enum Badge {
 
     PUSH_UP_BADGE("push up image url"),
+
     SQUAT_BADGE("push up image url");
+
+    static final int PERCENT_MULTIPLIER = 100;
 
     private final String imageUrl;
 
     Badge(final String imageUrl) {
         this.imageUrl = imageUrl;
+    }
+
+    public static int calculatePercent(final int count) {
+        return count / Badge.values().length * PERCENT_MULTIPLIER;
     }
 }
